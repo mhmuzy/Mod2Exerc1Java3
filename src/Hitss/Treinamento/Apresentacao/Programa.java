717af90d3e1dd565005a8bd419287bb3c538714c
marcio henrique <mhmuzy@embratel.com.br>
2021-05-21 13:40:52 -0300

    Criação do Programa [Treinamento]

---
+++ src/Hitss/Treinamento/Apresentacao/Programa.java
@@ -0,0 +1,10 @@
+package Hitss.Treinamento.Apresentacao;
+
+public class Programa {
+	public static void main(String[] args) {
+		System.out.println("");
+		System.out.println("        Hitss Treinamento");
+		System.out.println("");
+		System.out.println("Preazdo(a) Usuário(a), Estamos em Desenvolvimento no momento.");
+	}
+}
